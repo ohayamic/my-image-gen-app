081b7f94bf161d51f57bb12547acdbd029b2d587
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2024-04-07 22:10:28 +0200

    Mobile-view added

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,6 +1,83 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+
 const MobileNav = () => {
+  const pathname = usePathname();
+
   return (
-    <div>MobileNav</div>
-  )
-}
-export default MobileNav
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="Logo"
+          width={180}
+          height={28}
+        ></Image>
+      </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              ></Image>
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="Logo"
+                  width={152}
+                  height={23}
+                ></Image>
+                <ul className="header-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathname;
+                    return (
+                      <li
+                        key={link.route}
+                        className={`sidebar-nav_element group  ${
+                          isActive
+                            ? "bg-purple-gradient text-white"
+                            : "text-gray-700"
+                        }`}
+                      >
+                        <Link className="sidebar-link" href={link.route}>
+                          <Image
+                            src={link.icon}
+                            alt="logo"
+                            width={24}
+                            height={24}
+                            className={`${isActive && "brightness-200"}`}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </nav>
+    </header>
+  );
+};
+export default MobileNav;
